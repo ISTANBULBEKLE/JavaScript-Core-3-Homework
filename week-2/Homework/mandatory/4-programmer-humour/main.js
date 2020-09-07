68a60aa2e829ef8bec923942f97e89645e9e6110
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-07 16:20:05 +0100

    wee-2 4-programmer-humour is done

--- week-2/Homework/mandatory/4-programmer-humour/main.js
+++ week-2/Homework/mandatory/4-programmer-humour/main.js
@@ -6,6 +6,13 @@ fetch('https://xkcd.now.sh/?comic=latest')
         console.log(data);
         console.log(data.alt);
         console.log(data.img);
+
+        const titleElement = document.querySelector('#titleElement');
+        const imageElement = document.querySelector('#imageElement');
+        const altTextElement = document.querySelector('#altText');
+        titleElement.textContent = data.title;
+        imageElement.src = data.img;
+        altTextElement.textContent = ` " ${data.alt} "`;
     })
     .catch(function (err) {
         console.log(err);
