e7474cc5c3a37a07c077291bc24ce43b2249035e
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-09 17:46:41 +0100

    week-2 3-dog-photo is revised

--- week-2/Homework/mandatory/3-dog-photo-gallery/main.js
+++ week-2/Homework/mandatory/3-dog-photo-gallery/main.js
@@ -4,24 +4,22 @@
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(function (response) {
         return response.json();
+
     })
     .then(function (data) {
         console.log(data);
         console.log(data.message);
         const dogImage = data.message;
-        const beginTakingTheImage = '';
+        // const beginTakingTheImage = '';
 
         const selectButton = document.querySelector('#imageSelector');
         const selectUlElement = document.querySelector('.list-group');
-
-
 
         selectButton.addEventListener('click', selectDogImage);
 
         function selectDogImage(e) {
             const createLiElement = document.createElement('li');
             const createImgElement = document.createElement('img');
-            //
 
             createImgElement.src = dogImage;
             createImgElement.setAttribute = ('width', '50%');
@@ -29,10 +27,10 @@ fetch('https://dog.ceo/api/breeds/image/random')
             selectUlElement.appendChild(createLiElement);
             createLiElement.appendChild(createImgElement);
 
-            beginTakingTheImage = createImgElement;
+            //beginTakingTheImage = createImgElement;
 
         }
-        beginTakingTheImage.setAttribute = ('src', '');
+        // beginTakingTheImage.setAttribute = ('src', '');
     })
     .catch(function (err) {
         console.log(err);
