a3366c127e4e80b912ca00a05caf356983acc5e3
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-10 10:34:57 +0100

    week-2 3-dog-photo is revised

--- week-2/Homework/mandatory/3-dog-photo-gallery/main.js
+++ week-2/Homework/mandatory/3-dog-photo-gallery/main.js
@@ -1,37 +1,24 @@
 // function 
 
+const selectButton = document.querySelector('#imageSelector');
+const selectUlElement = document.querySelector('.list-group');
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(function (response) {
-        return response.json();
+selectButton.addEventListener('click', function () {
+    const selectLiElement = document.querySelector('.list-group-item');
+    const selectImgElement = document.querySelector('.imageElement');
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            selectImgElement.src = data.message;
+            selectUlElement.appendChild(selectLiElement);
+            selectLiElement.appendChild(selectImgElement);
+            selectImgElement.setAttribute = ('width', '50%');
+            selectImgElement.setAttribute = ('height', '40%');
 
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(data.message);
-        const dogImage = data.message;
-        // const beginTakingTheImage = '';
-
-        const selectButton = document.querySelector('#imageSelector');
-        const selectUlElement = document.querySelector('.list-group');
-
-        selectButton.addEventListener('click', selectDogImage);
-
-        function selectDogImage(e) {
-            const createLiElement = document.createElement('li');
-            const createImgElement = document.createElement('img');
-
-            createImgElement.src = dogImage;
-            createImgElement.setAttribute = ('width', '50%');
-            createImgElement.setAttribute = ('height', '40%');
-            selectUlElement.appendChild(createLiElement);
-            createLiElement.appendChild(createImgElement);
-
-            //beginTakingTheImage = createImgElement;
-
-        }
-        // beginTakingTheImage.setAttribute = ('src', '');
-    })
-    .catch(function (err) {
-        console.log(err);
-    })
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+})
